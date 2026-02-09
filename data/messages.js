1a3eecd606a215a509d65016ae04d3f22c6e36ce
jeremytey <jeremy8598@gmail.com>
2026-02-09 15:06:36 +0800

    finished sample messages data

--- data/messages.js
+++ data/messages.js
@@ -1,6 +1,6 @@
 const messages = [
-    { id: Date.now().toString(), messageUser: "Jackson",text: "Hello?", time: "10:00 AM" },
-    { id: (Date.now() + 1).toString(), messageUser: "You", text: "Hi, Jackson! How are you?", time: "10:01 AM" },
+    { id: '1', user: "Jackson",text: "Hello?", added: new Date() },
+    { id: '2', user: "Dylan", text: "Nigha u tripping", added: new Date() },
 ];
 
 module.exports = messages;
