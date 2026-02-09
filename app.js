69ff821b533d2b56a7111f8b3571b8c8fb19a734
jeremytey <jeremy8598@gmail.com>
2026-02-09 16:19:32 +0800

    push

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const messageRouter = require('./routes/messageRouter');
+
+// Set up view engine and middleware
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use('/', messageRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
